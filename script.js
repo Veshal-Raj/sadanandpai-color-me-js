27484520a5d95a172ec3c39778b9433d524047c9
veshalsamaaro <veshal@samaaro.com>
2024-11-29 18:08:43 +0530

    add: color me functionality completed

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('#color-me-input');
+const colorMeBtn = document.querySelector('#color-me-btn');
+const numbersDiv = document.querySelector('.numbers')
+
+colorMeBtn.addEventListener('click', activateNumber);
+
+function activateNumber() {
+    const inputVal = input.value;
+    if (!inputVal) {
+        alert('Please Enter a Number');
+        return;
+    }
+    if (inputVal < 1 || inputVal > 9) {
+        alert('Please Enter a Valid Number');
+        return;
+    }
+    const activeElement = document.querySelector('.num.active');
+    if (activeElement) {
+        activeElement.classList.remove('active');
+    }
+    const allElements = document.querySelectorAll('.num');
+    allElements[inputVal - 1].classList.add('active')
+}
